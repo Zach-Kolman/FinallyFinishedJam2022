36fdfff14ddc59f6c5a61bf1f83d744aec3229ab
wbondie <bondie.wes@gmail.com>
2022-01-14 17:55:54 -0600

    Added patrol view gizmo

--- Assets/AI/Patrol.cs
+++ Assets/AI/Patrol.cs
@@ -5,8 +5,15 @@ using UnityEngine.AI;
 
 public class Patrol : MonoBehaviour
 {
+    public bool DrawViewGizmo = false;
+    public bool DrawGizmoToFirstWaypoint = false;
     public Animator EnemyAnimator;
+    public Transform HeadJoint;
+    public float ViewDistance = 0.0f;
+    public float ViewAngle = 85.0f;
     public List<Waypoint> Waypoints;
+
+    private float MinViewAngle = 0.1f;
 
     private int waypointIdx;
     private Waypoint currentWaypoint;
@@ -14,19 +21,29 @@ public class Patrol : MonoBehaviour
     private float waitTimer;
     private bool isWaiting;
     private NavMeshAgent EnemyAgent;
-
     private string walkAnimVar = "IsWalking";
     private string velocityAnimVar = "Velocity";
 
     #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
+        if(DrawViewGizmo && HeadJoint)
+        {
+            Gizmos.color = new Color(1.0f, 1.0f, 0.5f, 0.3f);
+            Vector3 HeadPosition = new Vector3(HeadJoint.transform.position.x, HeadJoint.transform.position.y+0.15f, HeadJoint.transform.position.z);
+            Matrix4x4 HeadPositionMatrix = Matrix4x4.TRS(HeadPosition, HeadJoint.transform.rotation, HeadJoint.transform.localScale);
+            Matrix4x4 prevMatrix = Gizmos.matrix;
+            Gizmos.matrix = HeadPositionMatrix;
+            Gizmos.DrawFrustum(Vector3.zero, ViewAngle, ViewDistance, MinViewAngle, 1.0f);
+            Gizmos.matrix = prevMatrix;
+        }
+
         if(Waypoints.Count > 0)
         {
-            if(Waypoints.Count > 0 && currentWaypoint == Waypoints[0])
+            if(DrawGizmoToFirstWaypoint && Waypoints.Count > 0 && Waypoints[0])
             {
                 Gizmos.color = new Color(0.0f, 0.0f, 1.0f, 0.35f);
-                Gizmos.DrawLine(this.transform.position, currentWaypoint.transform.position);
+                Gizmos.DrawLine(this.transform.position, Waypoints[0].transform.position);
             }
             Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 0.35f);
             int idx = 1;
@@ -36,10 +53,6 @@ public class Patrol : MonoBehaviour
                 {
                     Gizmos.DrawLine(p.transform.position, Waypoints[idx].transform.position);
                     idx++;
-                }
-                else
-                {
-                    Gizmos.DrawLine(p.transform.position, Waypoints[0].transform.position);
                 }
             }
         }
